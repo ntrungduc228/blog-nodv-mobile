791b39155a2767f891a10743c15d856ca9bb96bf
Duc Nguyen <ntrungduc228@gmail.com>
2023-02-18 22:34:13 +0700

    Use js default for project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+  },
 };
