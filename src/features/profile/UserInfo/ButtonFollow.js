da57b7296310b3465c4444063d63ec6bb604b8d8
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-05-27 11:07:31 +0700

    Refactor ButtonFollow component and add Spinner component to ProfileScreen for loading state

--- src/features/profile/UserInfo/ButtonFollow.js
+++ src/features/profile/UserInfo/ButtonFollow.js
@@ -1,11 +1,11 @@
-import React from 'react';
 import {Button} from 'react-native-paper';
+import React from 'react';
 
 export const ButtonFollow = ({followed, onPress, ...props}) => {
   return (
     <Button
       style={{borderColor: '#4caf50'}}
-      className={`rounded-full mx-10 ${followed ? `bg-white` : `bg-[#4caf50]`}`}
+      className={`rounded-full ${followed ? `bg-white` : `bg-[#4caf50]`}`}
       mode="outlined"
       onPress={onPress}
       textColor={followed ? `#4caf50` : `#fff`}
